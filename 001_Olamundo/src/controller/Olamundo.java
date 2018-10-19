759ae9636df8c3372c7d8a54d1c0d4b3ca2bf3e8
João Fellipefelps <44007832+joaofellipe@users.noreply.github.com>
2018-10-18 21:10:02 -0300

    Add files via upload

---
+++ 001_Olamundo/src/controller/Olamundo.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class Olamundo {
+
+ /*
+ * Este é o seu primeiro programa em JAVA
+ */
+	public static void main(String[] ags) {
+         System.out.println("Olá mundo!");	
+         System.out.println(10);
+         }
+
+	
+ 
+}
