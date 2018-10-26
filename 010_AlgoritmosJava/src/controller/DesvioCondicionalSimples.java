c46a9e1b45a487fba0fccf3284f91e226240b748
João Fellipefelps <44007832+joaofellipe@users.noreply.github.com>
2018-10-25 21:36:44 -0300

    Add files via upload

---
+++ 010_AlgoritmosJava/src/controller/DesvioCondicionalSimples.java
@@ -0,0 +1,16 @@
+package controller;
+
+import util.Teclado;
+
+public class DesvioCondicionalSimples {
+	public static void main(String[] args) {
+		int a, b, x;
+		a = Teclado.lerInt("Informe o primeiro número:");
+		b = Teclado.lerInt("Infome o segundo número:");
+		x = a + b;
+		if (x > 10) {
+			System.out.println("O valor da soma é " + x);
+
+		}
+	}
+}
