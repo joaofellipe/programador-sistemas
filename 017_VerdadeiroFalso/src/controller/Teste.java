c09287978bc2e276ed2dda0d3fbd0222a0672a51
João Fellipefelps <44007832+joaofellipe@users.noreply.github.com>
2018-11-01 20:46:23 -0300

    Add files via upload

---
+++ 017_VerdadeiroFalso/src/controller/Teste.java
@@ -0,0 +1,17 @@
+package controller;
+
+public class Teste {
+	public static void main(String[] args) {
+		boolean b = false;
+		boolean a = true;
+		if ( a == b) {
+			System.out.println("Iguais");
+			
+		}
+		else
+		{
+			System.out.println("Diferentes");
+		}
+	}
+
+}
